cd78b8050cc034c262c9cb757ed43663bf056fdd
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2026-02-11 19:21:46 +0700

    test fetching

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Quiz = () => {
-  return (
-    <div>ini Quiz</div>
-  )
-}
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-export default Quiz
+  const fetchQuestions = async () => {
+    try {
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=5&category=9&type=multiple",
+      );
+
+      const data = await response.json();
+      console.log("HASIL API:", data);
+
+      setQuestions(data.results);
+    } catch (error) {
+      console.error(error);
+      setError("Gagal fetch data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div>
+      <h1>Quiz Page (Test Fetch)</h1>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      <ul>
+        {questions.map((q, i) => (
+          <li key={i}>{q.question}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Quiz;
