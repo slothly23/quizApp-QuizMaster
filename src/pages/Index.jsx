ac45c45b81a5e57d7a0a5ee798463dae4d76b9e2
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2026-02-13 15:00:04 +0700

    debug redirect

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,25 +1,27 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { loadUser } from "../utils/Storage";
+import { loadUser, hasResult } from "../utils/Storage";
 
 const Index = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const user = loadUser();
 
-    if (user) {
-      console.log("User ditemukan:", user);
-      navigate("/quiz");
-    } else {
-      console.log("Belum login");
+    if (!user) {
       navigate("/login");
+      return;
+    }
+
+    // kalau sudah punya hasil → ke result
+    if (hasResult()) {
+      navigate("/result");
+    } else {
+      navigate("/quiz");
     }
   }, []);
-  
-  return (
-    <p>Checking login...</p>
-  )
-}
 
-export default Index
+  return null;
+};
+
+export default Index;
